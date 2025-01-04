9abe2aeb58572e3678dcba368a879d2661668d5c
CH KARTHIKEYA <152009521+karthik7898@users.noreply.github.com>
2025-01-04 12:06:03 +0530

    Update demo4.java

--- Core Java/demo4.java
+++ Core Java/demo4.java
@@ -1,6 +1,6 @@
 public class demo4 {
     public static void main(String[] args) {
-        for(int i = 0; i<101; i ++){
+        for(int i = 0; i<=100; i ++){
             System.out.println(i);
         }
        
