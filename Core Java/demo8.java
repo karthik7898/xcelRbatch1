9383934886f5ac6fb9edcfd276ab5ce78ebcc3d4
CH KARTHIKEYA <152009521+karthik7898@users.noreply.github.com>
2025-01-06 21:52:07 +0530

    Add files via upload

---
+++ Core Java/demo8.java
@@ -0,0 +1,26 @@
+
+    
+import java.util.*;
+///public class Statements {
+ public class demo8 {
+    public static void main(String[] args) {
+        
+   Scanner sc = new Scanner(System.in);
+   System.out.print("Enter a number:");
+   int number = sc.nextInt();
+   if(number>0){
+    System.out.println("Entered number is positive");
+   }
+   else if (number<0) {
+   System.out.println("Entered number is negative");
+    
+   
+   }
+   else{
+    System.out.println("Entered number is zero");
+   }
+    }
+    
+}
+
+
