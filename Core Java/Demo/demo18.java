9d4bbb912060b53fce9c7a40f6f52c170472c487
CH KARTHIKEYA <152009521+karthik7898@users.noreply.github.com>
2025-01-09 10:14:37 +0530

    Add files via upload

---
+++ Core Java/Demo/demo18.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class demo18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter min: ");
+        int min = sc.nextInt();
+        double day = min/(24*60)%365;
+        double year = min/(365*24*60);
+    System.out.println("No of years for "+min +" is: " + year +" year");
+    System.out.println("no.of days " + min +" is "+ day + " day"); 
+    sc.close();  
+ }
+    
+}
