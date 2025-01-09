ed29f26dce352f708eb47023c105d8bf5c66a5c3
CH KARTHIKEYA <152009521+karthik7898@users.noreply.github.com>
2025-01-09 10:29:53 +0530

    Add files via upload

---
+++ Core Java/Demo/demo12.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class demo12{
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the the radius of the circle");
+        double radius = sc.nextDouble();
+        double PI = 3.14;
+        double area = PI*radius*radius;
+        System.out.println("Area of the circle is:"+area);
+        sc.close();
+
+    }
+}
