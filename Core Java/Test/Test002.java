f7d1e4c6e9eff1607daed881aeceebea91a7860f
CH KARTHIKEYA <152009521+karthik7898@users.noreply.github.com>
2025-01-10 19:37:36 +0530

    Add files via upload

---
+++ Core Java/Test/Test002.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+public class Test002 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+             String reply;
+             System.out.println("Enter reply:");
+             reply = sc.nextLine();
+    
+            while (reply.equals("yes")) {
+                System.out.println("Happy Sankranthi!");
+                System.out.println("Enter reply:");
+             reply = sc.nextLine();
+                
+               
+            }
+    
+            sc.close();
+        }
+    }
+    
+
+
+    
+    
