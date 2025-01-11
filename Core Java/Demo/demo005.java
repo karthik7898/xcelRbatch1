70cdb5a135dacb24d18d68697846c6cf9be45834
CH KARTHIKEYA <152009521+karthik7898@users.noreply.github.com>
2025-01-11 10:49:49 +0530

    Add files via upload

---
+++ Core Java/Demo/demo005.java
@@ -0,0 +1,14 @@
+
+public class demo005 {
+    public static void main(String[] args) {
+
+   String username = "IM BATMAN";
+   String pwd = "Im@batman";
+   while ((username=="IM BATMAN") && ( pwd=="Im@batman")){
+    System.out.println("Hello user, welcome");
+    break;
+    
+   } 
+    }
+    
+}
---
+++ Core Java/Demo/demo019.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+public class demo019 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of terms: ");
+        int n = sc.nextInt();
+        int first = 0;
+        int  second = 1;
+        System.out.print("Fibonacci Sequence:");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+        sc.close();
+    }
+}
+
+    
