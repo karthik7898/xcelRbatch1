efc23493754ec54775a6c8a457883dd251c3a229
CH KARTHIKEYA <152009521+karthik7898@users.noreply.github.com>
2025-01-21 17:51:27 +0530

    Greatest Number b/w 3 Digits

---
+++ Core Java/demo003.java
@@ -0,0 +1,20 @@
+
+public class demo003 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 9;
+        int c = 15 ;
+        if ((a>b) && (a>c)) {
+            System.out.println("a is greater than b and c");
+            
+        }
+        else if ((b>a) && (b>c)){
+            System.out.println("b is greater than a and c");
+        }
+        else{
+            System.out.println("c is greater than a & b");
+        }
+
+    }
+    
+}
