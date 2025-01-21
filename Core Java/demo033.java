61c15d70be232d6de83465cc332d50a5daf425c1
CH KARTHIKEYA <152009521+karthik7898@users.noreply.github.com>
2025-01-21 18:31:52 +0530

    Linked HashSet

---
+++ Core Java/demo033.java
@@ -0,0 +1,35 @@
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+class LinkedHashSetDemo {
+    void linkedHashSetMethod() {
+        LinkedHashSet<Integer> marksSet = new LinkedHashSet<>();
+        
+        
+        marksSet.add(85);
+        marksSet.add(90);
+        marksSet.add(75);
+        marksSet.add(90);  
+        marksSet.add(60);
+        marksSet.add(52);
+        
+ 
+        int sum = 0;
+        for (int mark : marksSet) {
+            sum += mark;
+        }
+        
+        System.out.println("Sum of marks: " + sum);
+        System.out.println("Marks in LinkedHashSet:");
+        Iterator<Integer> iterator = marksSet.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
+
+public class demo033 {
+    public static void main(String[] args) {
+        LinkedHashSetDemo obj = new LinkedHashSetDemo();
+        obj.linkedHashSetMethod();
+    }
+}
