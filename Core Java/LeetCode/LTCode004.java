e1bc5e5e0abc4d2d8c3389148d21bc2a0d1d4b65
CH KARTHIKEYA <152009521+karthik7898@users.noreply.github.com>
2025-01-23 11:26:57 +0530

    Add Two Numbers

---
+++ Core Java/LeetCode/LTCode004.java
@@ -0,0 +1,50 @@
+package LeetCode;
+
+class LTCode004 {
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            carry = sum / 10;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        LTCode004 solution = new LTCode004();
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        
+        ListNode result = solution.addTwoNumbers(l1, l2);
+        printList(result);
+    }
+
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val);
+            if (node.next != null) System.out.print(" -> ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+}
