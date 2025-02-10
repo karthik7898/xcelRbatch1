8c6896cfd8be7974d06a40b7eac50418a399184b
CH KARTHIKEYA <152009521+karthik7898@users.noreply.github.com>
2025-02-10 11:36:16 +0530

    Multiplication Table of a Number

---
+++ Core Java/demo050.java
@@ -0,0 +1,13 @@
+public class demo050 {
+    public static void main(String[] args) {
+        
+    
+
+    int n = 5;
+           System.out.println("Table of "+n);
+           for (int i = 0; i <= 10; i++) {
+               System.out.printf("%d X %d = %d\n",n,i,i*n);
+           }
+    
+}
+}
